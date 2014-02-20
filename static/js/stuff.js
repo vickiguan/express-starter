5ecff85445a245b37bc71402cd0e38efa9fd4813
vickiguan <vickiguan@berkeley.edu>
2014-02-19 19:09:15 -0800

    stuff

---
+++ static/js/stuff.js
@@ -0,0 +1,24 @@
+alert('Welcome to my stuff page!'); // edit me!
+
+// Problem 1 (Say Hello!) ---------------------------------------------------
+$('#say_hello').click(function(alert('Hello Whirled!')) {
+  // WRITE CODE HERE
+});
+
+
+// Problem 2 (Houdini) ------------------------------------------------------
+$('#disappear').click(function() {
+  //WRITE CODE HERE
+});
+
+$('#reappear').click(function() {
+  //WRITE CODE HERE
+});
+
+
+// Problem 3 (Tickle Me Pink) -----------------------------------------------
+// WRITE CODE HERE
+
+
+// Problem 4 (Greet Me) -----------------------------------------------------
+// WRITE CODE HERE
