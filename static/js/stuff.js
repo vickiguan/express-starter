9151e9f02fa420637f06c57fef6cbe86b309a96f
vickiguan <vickiguan@berkeley.edu>
2014-02-19 19:33:04 -0800

    stuff

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -18,7 +18,7 @@ $('#reappear').click(function() {
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
 $('#tickleMePink').click(function() {
-  $('#problem_title')css('color','pink');
+  $('#tickleMePink')css('color','pink');
 });
 
 
