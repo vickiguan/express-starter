0aea40344910a13f3718ec33074341b6f8d03f44
vickiguan <vickiguan@berkeley.edu>
2014-02-19 19:56:48 -0800

    stuff

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -24,6 +24,6 @@ $('#tickleMePink').click(function() {
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 $('#greetMe').click(function() {
-  alert('Hey'+$('#my_name').val());
+  alert('Hey '+$('#my_name').val());
 });
 
