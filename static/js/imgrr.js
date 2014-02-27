37d7cd0d63c50692ca0e4b21612c32bebec19800
vickiguan <vickiguan@berkeley.edu>
2014-02-26 19:06:43 -0800

    Completed Animals problem!

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,7 +1,9 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-$('#toggle_img').click(function($('#toggle_img').text('Come Back!')) {
+$('#toggle_img').click(function() {
   $('#main_img').hide();
 });
+
+$('#toggle_img').text('Come Back!')
 
 $('#toggle_img').click(function() {
   $('#main_img').show();
