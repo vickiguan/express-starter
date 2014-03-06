3ea756d14b16148e1cd8dc298c0792d42a8cb97f
vickiguan <vickiguan@berkeley.edu>
2014-03-05 18:50:13 -0800

    mawr lines

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -18,4 +18,26 @@ $(document).ready(function() {
   	context.strokeRect(100, 20, 20, 20)
   });
 
+  $('#p3').click(function() {
+  	context.beginPath()
+  	context.arc(30, 50, 15,0, Math.PI)
+  	context.stroke()
+  });
+  
+  $('#p4').click(function() {
+  	context.beginPath()
+  	context.arc(50, 50, 15,0, Math.PI)
+  	context.arc(50, 50, 15, Math.PI, 0)
+  	context.closePath()
+  	context.stroke()
+  });
+  
+  $('#p5').click(function() {
+  	context.beginPath()
+  	context.moveTo(60,60)
+  	context.lineTo(90,90)
+  	context.closePath()
+  	context.stroke()
+  });
+
 });
